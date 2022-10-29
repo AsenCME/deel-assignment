a8b24803687b70ca0502e108a64741a7a3ff84a2
Asen Georgiev <asen.connor@abv.bg>
2022-10-29 16:42:40 +0300

    better index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
-  return <h1>Index page!</h1>;
+  return (
+    <>
+      <h1>Hello!</h1>
+      <p>Welcome to my app. You will find all information in the README.md</p>
+    </>
+  );
 }
